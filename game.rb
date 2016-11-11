282e088457559c5b5c6c2451588bd07e8e3d5bb2
gaber79 <poetdreams@hotmail.com>
2016-11-11 19:33:33 +0000

    completed game with no errors

--- game.rb
+++ game.rb
@@ -4,13 +4,13 @@ class Game
 
   def initialize
     puts "Enter first player name: "
-    player1name = gets.chomp
-    @player1 = Player.new(player1name)
+    # player1name = gets.chomp
+    @player1 = Player.new(gets.chomp)
     puts "Enter second player name: "
-    player2name = gets.chomp
-    @player2 = Player.new(player2name)
+    # player2name = gets.chomp
+    @player2 = Player.new(gets.chomp)
     @current_player = @player1
-    puts "#{@current_player.name}"
+    # puts "Initialized name current  #{@current_player.name}"
   end
 
   def start
@@ -23,28 +23,34 @@ class Game
   def switch_player
     if @current_player == @player1
       @current_player = @player2
+      puts "Switched to ... #{@current_player.name}"
     else
       @current_player = @player1
+      puts "Switched to ... #{@current_player.name}"
     end
   end
 
   def ask_question
 
     q = Question.new
-    q.generate_question
+    puts
+    puts "#{current_player.name} it is your turn."
+    puts "#{q.generate_question}"
     player_answer = gets.chomp
     result = q.verify_answer(player_answer)
 
     if result == true
-      puts "good job! fluke though!"
+      puts "That is correct!! Good job! Fluke though!"
       puts "#{@current_player.name} score: #{current_player.lives}"
     else
-      puts "bad job! why are you in this program?!"
-      p = @current_player.lives
-      puts p
-      p -= 1
-      puts p
-      puts "#{current_player.name} score: #{current_player.lives}"
+      puts
+      puts "You are wrong!! Leonidas! Bad job! Why are you in this program?!"
+      # puts 
+      @current_player.lives -= 1
+      puts
+      puts "#{current_player.name}'s score: #{current_player.lives}"
+      puts
+
     end
 
     switch_player
